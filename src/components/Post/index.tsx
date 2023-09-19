a43fac3b9adb9814e70922414c13fc113364eebf
Davi <davi@MBP-de-Diana.lan>
2023-09-19 16:01:00 -0300

    Finished my PostHeader

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -3,7 +3,7 @@ import { View, Text, Image } from "react-native";
 import { Divider } from 'react-native-elements';
 
 interface PostProps {
-  post: { 
+  post: {
     imageURL: string;
     user: string;
   };
@@ -11,11 +11,11 @@ interface PostProps {
 
 const Post: React.FC<PostProps> = ({ post }) => {
   return (
-    <View style={{ marginBottom: 30, marginLeft: 10 }}>
+    <View style={{ marginLeft: 10 }}>
       <Divider
         width={1}
         orientation="vertical"
-        style={{ marginBottom: 20 }} />
+        style={{ marginBottom: 5 }} />
       <PostHeader post={post} />
 
     </View>
@@ -24,18 +24,37 @@ const Post: React.FC<PostProps> = ({ post }) => {
 
 const PostHeader: React.FC<PostProps> = ({ post }) => {
   return (
-    <View 
-    style={{ 
-    alignItems: 'center', 
-    justifyContent: 'space-between', 
-    flexDirection: 'row', 
-    margin: 5 }}
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        margin: 5
+      }}
     >
       <Image
         source={{ uri: post.imageURL }}
-        style={{ width: 35, height: 35 }} />
+        style=
+        {{
+          width: 35,
+          height: 35,
+          borderWidth: 1,
+          borderColor: '#F7A34B',
+          borderRadius: 50,
+        }}
+      />
       <Text
-        style={{ color: 'black' }}>{post.user}</Text>
+        style=
+        {{ 
+        color: 'black', 
+        fontWeight: '600',
+        paddingRight: 280 
+        }}
+        >{post.user}
+        </Text>
+        <View>
+          <Text style={{ color: 'black', fontWeight: '900', marginRight: 8 }}>...</Text>
+        </View>
     </View>
   );
 }
